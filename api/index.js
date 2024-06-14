b3fa6c603aff9dfcebdfb76bfeaad363dd46e7b8
Ritik Sharma <ritiksharma9307@gmail.com>
2024-06-14 20:18:21 +0530

    adding backend modules

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect()
+
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000!!");
+});
